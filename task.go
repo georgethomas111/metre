f3b7f9f2944e4b1a9f3dd0e06cee7543fff710fd
george <george.thomas@hart.com>
2016-07-26 17:36:53 -0700

    In between redesign

--- task.go
+++ task.go
@@ -1,8 +1,21 @@
 package metre
 
 type Task struct {
-    ID string // Type Type of task (user as class prefix in cache)
-    Interval string // Schedule String in cron notation
-    Schedule func(t TaskRecord, s Scheduler, c Cache, q Queue)
-    Process func(t TaskRecord, s Scheduler, c Cache, q Queue)
+	// All the messages and synchronization info happens here.
+	MessageChan chan string
+
+	// Type Type of task (user as class prefix in cache).
+	ID string
+
+	// Schedule String in cron notation.
+	Interval string
+
+	// Schedules a task for execution by slave.
+	Schedule func(t TaskRecord, s Scheduler, c Cache, q Queue)
+
+	// The process action where slave picks up the part that is given to it.
+	Process func(t TaskRecord, s Scheduler, c Cache, q Queue)
+
+	// Track the completion and sends updates for a task.
+	Track func() chan string
 }
