31a7677cc7d1bc4e6068a9762e861ebe2624b4ea
george <george.thomas@hart.com>
2016-08-10 17:35:43 -0700

    Adding simple messaging facility in the metre

--- task.go
+++ task.go
@@ -1,8 +1,21 @@
 package metre
 
 type Task struct {
-    ID string // Type Type of task (user as class prefix in cache)
-    Interval string // Schedule String in cron notation
-    Schedule func(t TaskRecord, s Scheduler, c Cache, q Queue)
-    Process func(t TaskRecord, s Scheduler, c Cache, q Queue)
+	ID       string // Type Type of task (user as class prefix in cache)
+	Interval string // Schedule String in cron notation
+	Schedule func(t TaskRecord, s Scheduler, c Cache, q Queue)
+	Process  func(t TaskRecord, s Scheduler, c Cache, q Queue)
+}
+
+func (t Task) GetID() string {
+	return t.ID
+}
+
+func (t Task) GetInterval() string {
+	return t.Interval
+}
+
+type TaskInt interface {
+	GetID() string
+	GetInterval() string
 }
